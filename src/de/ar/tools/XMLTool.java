49ec796ff72ec39ae101b51c825833b332cbd2df
Shadowmore <alexander.rabel92@gmail.com>
2015-06-01 16:11:41 +0200

    Javadoc for XMLTool

--- src/de/ar/tools/XMLTool.java
+++ src/de/ar/tools/XMLTool.java
@@ -15,14 +15,35 @@ import org.jdom2.input.SAXBuilder;
 import org.jdom2.output.Format;
 import org.jdom2.output.XMLOutputter;
 
+/**
+ * 
+ * Tool for XML operations
+ * 
+ * @author Shadowmore
+ *
+ */
 public class XMLTool {
 
 	private static SAXBuilder saxBuilder = new SAXBuilder();
 
+	/**
+	 * 
+	 * Creates an empty Document with a root-element named "ROOT"
+	 * 
+	 * @return Document
+	 */
 	public static Document createEmptyXMLDoc() {
 		return createEmptyXMLDoc("ROOT");
 	}
 
+	/**
+	 * 
+	 * Creates an empty Document with a root-element named by the parameter rootName
+	 * 
+	 * @param rootName
+	 *            Root-element name
+	 * @return Document
+	 */
 	public static Document createEmptyXMLDoc(String rootName) {
 		Document xmlDoc = new Document();
 		Element rootNode = new Element(rootName);
@@ -30,21 +51,52 @@ public class XMLTool {
 		return xmlDoc;
 	}
 
+	/**
+	 * 
+	 * Adds a new child-element to the parent-element(<b>parentNode</b>) with the given name(<b>nodeName</b>)
+	 * 
+	 * @param parentNode
+	 *            Parent-element
+	 * @param nodeName
+	 *            Child-element name
+	 * @return new Child-element
+	 */
 	public static Element addChildNode(Element parentNode, String nodeName) {
 		Element childNode = new Element(nodeName);
 		parentNode.addContent(childNode);
 		return childNode;
 	}
 
+	/**
+	 * 
+	 * Adds a new child-element to the parent-element(<b>parentNode</b>) with the given name(<b>nodeName</b>) and the given text(<b>nodeText</b>)
+	 * 
+	 * @param parentNode
+	 *            Parent-element
+	 * @param nodeName
+	 *            Child-element name
+	 * @param nodeText
+	 *            Child-element text
+	 * @return new Child-element
+	 */
 	public static Element addChildNode(Element parentNode, String nodeName, String nodeText) {
 		Element childNode = addChildNode(parentNode, nodeName);
 		childNode.addContent(nodeText);
 		return childNode;
 	}
 
-	public static void saveXMLDocumentToFile(Document xmlDoc, String FullFilename) {
+	/**
+	 * 
+	 * Save the document(<b>xmlDoc</b>) the the given path(<b>fullFilename</b>)
+	 * 
+	 * @param xmlDoc
+	 *            Document to save to
+	 * @param fullFilename
+	 *            Path and file name to save the document to
+	 */
+	public static void saveXMLDocumentToFile(Document xmlDoc, String fullFilename) {
 		try {
-			FileOutputStream fileOutputStream = new FileOutputStream(new File(FullFilename));
+			FileOutputStream fileOutputStream = new FileOutputStream(new File(fullFilename));
 			XMLOutputter xmlOutputter = new XMLOutputter();
 
 			Format myFormat;
@@ -62,6 +114,15 @@ public class XMLTool {
 		}
 	}
 
+	/**
+	 * 
+	 * Save the document(<b>xmlDoc</b>) the the given path(<b>fullFilename</b>) with a nice style
+	 * 
+	 * @param xmlDoc
+	 *            Document to save to
+	 * @param fullFilename
+	 *            Path and file name to save the document to
+	 */
 	public static void saveXMLDocumentToFileWithIndent(Document xmlDoc, String FullFilename) {
 		try {
 			FileOutputStream fileOutputStream = new FileOutputStream(new File(FullFilename));
@@ -79,23 +140,40 @@ public class XMLTool {
 		}
 	}
 
+	/**
+	 * 
+	 * Loads a xml document from the given url(<b>xmlURL</b>)
+	 * 
+	 * @param xmlURL
+	 *            Url to load from
+	 * @return Document
+	 */
 	public static Document loadXMLFile(URL xmlURL) {
-		Document xmlDoc = null;
-		SAXBuilder saxBuilder = new SAXBuilder();
-
-		try {
-			xmlDoc = saxBuilder.build(xmlURL);
-		} catch (JDOMException e) {
-			throw new RuntimeException(e);
-		} catch (IOException e) {
-			throw new RuntimeException(e);
-		}
-		return xmlDoc;
+		return loadXMLFile(xmlURL.getPath());
 	}
 
+	/**
+	 * 
+	 * Loads a xml document from the given file(<b>xmlFile</b>)
+	 * 
+	 * @param xmlFile
+	 *            File to load from
+	 * @return Document
+	 */
+	public static Document loadXMLFile(File xmlFile) {
+		return loadXMLFile(xmlFile.getAbsolutePath());
+	}
+
+	/**
+	 * 
+	 * Loads a xml document from the given filename(<b>xmlFilename</b>)
+	 * 
+	 * @param xmlFilename
+	 *            Filename to load from
+	 * @return Document
+	 */
 	public static Document loadXMLFile(String xmlFilename) {
 		Document xmlDoc = null;
-		SAXBuilder saxBuilder = new SAXBuilder();
 
 		try {
 			xmlDoc = saxBuilder.build(xmlFilename);
@@ -107,29 +185,15 @@ public class XMLTool {
 		return xmlDoc;
 	}
 
-	public static Document loadXMLFile(File xmlFile) {
-		Document xmlDoc = null;
-		SAXBuilder saxBuilder = new SAXBuilder();
-
-		try {
-			xmlDoc = saxBuilder.build(xmlFile);
-		} catch (JDOMException e) {
-			throw new RuntimeException(e);
-		} catch (IOException e) {
-			throw new RuntimeException(e);
-		}
-		return xmlDoc;
-	}
-
-	public static boolean isXmlStringValid(String xmlDocString) throws Exception {
-		Document xmlDoc = null;
-		SAXBuilder saxBuilder = new SAXBuilder();
-		StringReader MyStringReader = new StringReader(xmlDocString);
-		xmlDoc = saxBuilder.build(MyStringReader);
-		return (xmlDoc != null); // => true or Exception;
-	}
-
-	public synchronized static Document loadXML(String xmlDocString) {
+	/**
+	 * 
+	 * Converts the given String(<b>xmlDocString</b>) into a Document
+	 * 
+	 * @param xmlDocString
+	 *            String to convert to
+	 * @return Document
+	 */
+	public synchronized static Document convertStringtoXML(String xmlDocString) {
 		Document xmlDoc = null;
 		StringReader MyStringReader = new StringReader(xmlDocString);
 
@@ -143,30 +207,34 @@ public class XMLTool {
 		return xmlDoc;
 	}
 
-	public static String getXmlAsStr(Element Node) {
-
-		String resultXmlString;
-		XMLOutputter xmlOutputter = new XMLOutputter();
-
-		Format myFormat;
-		myFormat = xmlOutputter.getFormat();
-		myFormat.setOmitEncoding(true);
-		myFormat.setOmitDeclaration(true);
-		xmlOutputter.setFormat(myFormat);
-
-		try {
-			resultXmlString = xmlOutputter.outputString(Node);
-		} catch (RuntimeException e) {
-			resultXmlString = null;
-		}
-		return resultXmlString;
+	/**
+	 * 
+	 * Checks if the given string(<b>xmlDocString</b>) is xml compatible
+	 * 
+	 * @param xmlDocString
+	 *            String to check
+	 * @return
+	 * @throws Exception
+	 */
+	public static boolean isXmlStringValid(String xmlDocString) throws Exception {
+		Document xmlDoc = null;
+		SAXBuilder saxBuilder = new SAXBuilder();
+		StringReader MyStringReader = new StringReader(xmlDocString);
+		xmlDoc = saxBuilder.build(MyStringReader);
+		return (xmlDoc != null); // => true or Exception;
 	}
 
-	public static String getXmlAsStr(Document xmlDoc) {
-		return getXmlAsStr(xmlDoc.getRootElement());
-	}
-
-	public static String getXmlAsStr(Element Node, boolean pretty) {
+	/**
+	 *
+	 * Converts the given Element(<b>node</b>) into a String
+	 * 
+	 * @param node
+	 *            Element to convert to
+	 * @param pretty
+	 *            Pretty or compact format
+	 * @return XML as String
+	 */
+	public static String getXmlAsStr(Element node, boolean pretty) {
 		String resultXmlString;
 		XMLOutputter xmlOutputter = new XMLOutputter();
 
@@ -177,13 +245,22 @@ public class XMLTool {
 		}
 
 		try {
-			resultXmlString = xmlOutputter.outputString(Node);
+			resultXmlString = xmlOutputter.outputString(node);
 		} catch (RuntimeException e) {
 			resultXmlString = null;
 		}
 		return resultXmlString;
 	}
 
+	/**
+	 * 
+	 * Merges two Documents into one
+	 * 
+	 * @param xmlDocMergeDest
+	 *            XML to merge into
+	 * @param xmlDocMergeSource
+	 *            XML to merge from
+	 */
 	@SuppressWarnings("rawtypes")
 	public static void mergeXmlDoc(Document xmlDocMergeDest, Document xmlDocMergeSource) {
 		for (ListIterator it = xmlDocMergeSource.getRootElement().getChildren().listIterator(); it.hasNext();) {
